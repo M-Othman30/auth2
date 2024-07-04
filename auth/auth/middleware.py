5c61c7c26d093dd45f001a322d4759468cc9ab76
Mohanad Alashry <96386074+3ashryy2@users.noreply.github.com>
2024-07-04 16:43:12 +0300

    Update middleware.py

--- auth/auth/middleware.py
+++ auth/auth/middleware.py
@@ -43,7 +43,7 @@ class APIGatewayMiddleware:
             return response
         except Exception as e:
             # logger.exception('Error in API Gateway Middleware: %s', str(e))
-            return self._error_response('Internal Server Error')
+            return self._error_response('Internal Server Error here1')
 
 
     def authorize_request(self, request):
