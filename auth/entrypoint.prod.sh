6da7a4968d4db05337d9709b21c233e4e057564d
محمد عبدالرحمن محمد عثمان <115634617+M-Othman30@users.noreply.github.com>
2024-07-05 23:29:05 +0300

    test

--- auth/entrypoint.prod.sh
+++ auth/entrypoint.prod.sh
@@ -12,8 +12,8 @@ then
 fi
 cd ./auth
 python manage.py flush --no-input
-python manage.py makemigrations
-python manage.py migrate
+#python manage.py makemigrations
+#python manage.py migrate
 gunicorn auth.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
